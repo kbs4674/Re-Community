d7c8aaac05453de9836d31c4a8dbf3389d408643
ChulMin Kim <kbs4674@likelion.org>
2018-01-31 14:10:37 +0000

    내 정보함 수정

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,20 +1,20 @@
 class HomesController < ApplicationController
     def index
         @bulletins = Bulletin.all
-        @posts_all = Post.order("created_at DESC").first 5
-        @notices_all = AllNotice.order("created_at DESC").first 5
+        @posts_all = Post.order("created_at DESC").page(params[:page]).per(6)
+        @notices_all = AllNotice.order("created_at DESC").page(params[:page]).per(5)
         @posts = Post.order("created_at DESC").page params[:page]
-        @qnas = Qna.order("created_at DESC").first 5
+        @qnas = Qna.order("created_at DESC").page(params[:page]).per(5)
         
         @hot_posts = Post.all
-        @hot_posts = Post.order("impressions_count DESC").first 5
+        @hot_posts = Post.order("impressions_count DESC").page(params[:page]).per(5)
         
-        @hot_post_likes = Post.order(:cached_votes_up => :desc).first 5
+        @hot_post_likes = Post.order(:cached_votes_up => :desc).page(params[:page]).per(5)
         
         @hot_qnas = Qna.all
-        @hot_qnas = Qna.order(:cached_votes_up => :desc).first 5
+        @hot_qnas = Qna.order(:cached_votes_up => :desc).page(params[:page]).per(5)
         
         @qna_all = Qna.all
-        @hot_qna_replys = CommentQna.order(:cached_votes_up => :desc).first 5
+        @hot_qna_replys = CommentQna.order(:cached_votes_up => :desc).page(params[:page]).per(5)
     end
 end
--- app/models/home.rb
+++
@@ -1,6 +0,0 @@
-class Home < ApplicationRecord
-    has_many :posts
-    has_many :qnas
-    has_many :comments
-    has_many :comment_qnas
-end
