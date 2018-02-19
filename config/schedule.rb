aa2bace5ad6b8f96197ed2239af404b922c4309d
ChulMin Kim <kbs4674@likelion.org>
2018-02-19 20:30:45 +0000

    DB(PostgreSQL) 백업 기능 추가

--- config/schedule.rb
+++ config/schedule.rb
@@ -8,17 +8,24 @@
 # set :output, "/path/to/my/cron_log.log"
 #
 
- every 30.day, :at => '4:30 am' do
+# [1년 단위] 이미 삭제된 게시글 완전히 삭제
+every 30.day, :at => '4:30 am' do
 #   command "/usr/bin/some_great_command"
 #   runner "MyModel.some_method"
     rake "posts:delete_all_post_1year"
- end
+end
  
- every 7.weeks, :at => '4:30 am' do
+# [1주일 단위] 네이버 로그인기록 삭제
+every 7.weeks, :at => '4:30 am' do
 #   command "/usr/bin/some_great_command"
 #   runner "MyModel.some_method"
     rake "naver_login_delete:delete_naver_login_1weeks"
- end
+end
+ 
+# [1주일 단위] 자료 백업
+every 7.weeks, :at => '4:50 am' do
+    command "backup perform --trigger knu_backup"
+end
 
 # every 4.days do
 #   runner "AnotherModel.prune_old_records"
