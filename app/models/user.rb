b01a26b0f2f37ba2501770a1abe577afff4b36bf
ChulMin Kim <kbs4674@likelion.org>
2018-02-22 19:12:57 +0000

    user.rb 수정

--- app/models/user.rb
+++ app/models/user.rb
@@ -69,7 +69,7 @@ class User < ApplicationRecord
           user = User.new(
             name: auth.info.name,
             email: email ? auth.info.email : "#{auth.uid}@#{auth.provider}.com",
-            nickname: "#{identity.uid}@#{auth.provider}",
+            nickname: "#{auth.info.email}",
             password: Devise.friendly_token[0,20]
           )            
           user.save!
