26f4b60d68c268f1d8e8f6e894b3dc5fba133eb6
ChulMin Kim <kbs4674@likelion.org>
2018-02-26 04:47:57 +0000

    메인 수정중

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,29 +35,30 @@ humor = Bulletin.create( title: "유머게시판", content: "바나나가 웃으
 group = Bulletin.create( title: "동아리 홍보 게시판", content: "동아리 홍보 전용 게시판 입니다!", user_id: "1" ) #post id : 3
 foreigner = Bulletin.create( title: "Foreigner Board", content: "board For Foreigner Bulletin Board, Please use not only Korean, also your language", user_id: "1" ) #post id : 4
 vote = Bulletin.create( title: "토론장", content: "어떤 한 주제에 대해 찬/반을 가려가며 논의하는 게시판입니다.<br/>'controversia'는 라틴어로 '논쟁' 이란 뜻이 있음.", user_id: "1", vote: "true" ) #post id : 5
-request = Bulletin.create( title: "홈페이지 피드백 게시판", content: "홈페이지 피드백에 대해 남길 수 있는 게시판", main_hidden: "true", user_id: "1" ) #post id : 6
 
 #익명
-nonamed = Bulletin.create( title: "익명 자유게시판", content: "익명 자유게시판", user_id: "1", info_no_named: "true", main_hidden: "true" ) #post id : 7
-love = Bulletin.create( title: "연애상담 게시판", content: "연애상담 게시판 입니다.", user_id: "1", main_hidden: "true", info_no_named: "true" ) #post id : 8
-get_it_out = Bulletin.create( title: "고민고백 게시판", content: "고민고백 게시판 입니다.", user_id: "1", main_hidden: "true", info_no_named: "true" ) #post id : 9
+nonamed = Bulletin.create( title: "익명 자유게시판", content: "익명 자유게시판", user_id: "1", info_no_named: "true" ) #post id : 6
+love = Bulletin.create( title: "연애상담 게시판", content: "연애상담 게시판 입니다.", user_id: "1", info_no_named: "true" ) #post id : 7
+get_it_out = Bulletin.create( title: "고민고백 게시판", content: "고민고백 게시판 입니다.", user_id: "1", info_no_named: "true" ) #post id : 8
 
 #명소가게 탐방
-store1 = Bulletin.create( title: "우리가게 홍보", content: "사장님들이 직접 홍보하는 가게입니다.", user_id: "1", promote_store: "true", main_hidden: "true" ) #post id : 10
-store2 = Bulletin.create( title: "가게 방문후기", content: "맛집탐방, 명소가게 후기", user_id: "1" ) #post id : 11
+store1 = Bulletin.create( title: "우리가게 홍보", content: "사장님들이 직접 홍보하는 가게입니다.", user_id: "1", promote_store: "true", main_hidden: "true" ) #post id : 9
+store2 = Bulletin.create( title: "가게 방문후기", content: "맛집탐방, 명소가게 후기", user_id: "1" ) #post id : 10
 
 #구인구직
-job_find1 = Bulletin.create( title: "일자리 : 구인", content: "일자리에 적합한 사람을 구하는 게시판", user_id: "1", search_job: "true" ) #post id : 12
-job_find2 = Bulletin.create( title: "일자리 : 구직", content: "아르바이트 혹은 일자리 알아보는 게시판", user_id: "1", seeking_job: "true", main_hidden: "true" ) #post id : 13
+job_find1 = Bulletin.create( title: "일자리 : 구인", content: "일자리에 적합한 사람을 구하는 게시판", user_id: "1", search_job: "true" ) #post id : 11
+job_find2 = Bulletin.create( title: "일자리 : 구직", content: "아르바이트 혹은 일자리 알아보는 게시판", user_id: "1", seeking_job: "true", main_hidden: "true" ) #post id : 12
 
 #거래
-room_sell = Bulletin.create( title: "복덕방", content: "방 정보 게시판", user_id: "1", info_room_trade: "true", main_hidden: "true" ) #post id : 14
-room_find = Bulletin.create( title: "방 구해요", content: "학생들이 방을 구하는 게시판.", user_id: "1", seeking_room: "true", main_hidden: "true" ) #post id : 15
-trade = Bulletin.create( title: "아나바다", content: "중고거래 게시판", user_id: "1", info_trade: "true" ) #post id : 16
+room_sell = Bulletin.create( title: "복덕방", content: "방 정보 게시판", user_id: "1", info_room_trade: "true", main_hidden: "true" ) #post id : 13
+room_find = Bulletin.create( title: "방 구해요", content: "학생들이 방을 구하는 게시판.", user_id: "1", seeking_room: "true", main_hidden: "true" ) #post id : 14
+trade = Bulletin.create( title: "아나바다", content: "중고거래 게시판", user_id: "1", info_trade: "true" ) #post id : 15
 
 #비공개 게시판
-for_boss = Bulletin.create( title: "사장님 공지사항", content: "사장님들께 소식을 전해드리는 게시판.", user_id: "1",  main_hidden: "true", member_boss: "true" ) #post id : 17
-for_boss = Bulletin.create( title: "학교 관계자 공지사항", content: "학교 관계자 분들께 소식을 전해드리는 게시판.", user_id: "1", main_hidden: "true", member_univ: "true" ) #post id : 18
+for_boss = Bulletin.create( title: "사장님 공지사항", content: "사장님들께 소식을 전해드리는 게시판.", user_id: "1",  main_hidden: "true", member_boss: "true" ) #post id : 16
+for_boss = Bulletin.create( title: "학교 관계자 공지사항", content: "학교 관계자 분들께 소식을 전해드리는 게시판.", user_id: "1", main_hidden: "true", member_univ: "true" ) #post id : 17
+
+request = Bulletin.create( title: "홈페이지 피드백 게시판", content: "홈페이지 피드백에 대해 남길 수 있는 게시판", main_hidden: "true", user_id: "1" ) #post id : 18
 
 #게시글 작성
 notice1 = AllNotice.create( title: "안녕하세요, 관리자 입니다.", content: "현재 이 홈페이지는..<br/>아직 개발중인 홈페이지다보니 홈페이지 내 버그가 있거나, 매번 데이터가 예고없이 삭제가 될 수 있습니다.<br/><br/>이 점 양해 부탁드립니다.", user_id: "1", notice: "false", all_notice: "true", comment_permission: "true", title_tag_permission: "true")
