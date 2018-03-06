f0612b191a8ee320e73ae3aa432289af70dc0694
ChulMin Kim <kbs4674@likelion.org>
2018-03-06 15:47:53 +0000

    test

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,9 @@
+require "tzinfo"
+
+def local(time)
+        TZInfo::Timezone.get('Asia/Seoul').local_to_utc(Time.parse(time))
+end
+
 # Use this file to easily define all of your cron jobs.
 #
 # It's helpful, but not entirely necessary to understand cron before proceeding.
@@ -8,49 +14,49 @@
 # set :output, "/path/to/my/cron_log.log"
 #
 
-# [1년 단위] 이미 삭제된 게시글 완전히 삭제
-every 1.month, :at => '4:30 am' do
+# [1달 단위] 1년 전 이미 삭제된 게시글 완전히 삭제
+every 1.month, at: local('4:25 am') do
 #   command "/usr/bin/some_great_command"
 #   runner "MyModel.some_method"
     rake "posts:delete_all_post_1year"
 end
 
 # [1주일 단위] 네이버 로그인기록 삭제
-every 7.days, :at => '4:30 am' do
+every 7.days, at: local('4:30 am') do
 #   command "/usr/bin/some_great_command"
 #   runner "MyModel.some_method"
     rake "naver_login_delete:delete_naver_login_1weeks"
 end
 
 # [3일 단위] 자료 백업
-every 3.days, :at => '4:50 am' do
+every 3.days, at: local('4:50 am') do
 #every 1.minutes do
     command "cd /home/ec2-user/knu;backup perform --trigger weekly_db_backup"
 end
 
 # [3일 단위] 백업된 자료 깃에 보내기
-every 3.days, :at => '4:55 am' do
+every 3.days, at: local('4:55 am') do
 #every 2.minutes do
     command "cd /home/ec2-user/weekly_db_backup;git add .;git commit -m 'weekly_db_backup';git push -u db master"
 end
 
 # [매일] 식사 최신화
-every :day, at: ['11:59 pm', '09:01 am', '14:01 pm'] do
+every :day, at: [local('11:59 pm'), local('09:01 am'), local('14:01 pm')] do
     rake "crawling_meal:crawling_meal"
 end
 
 # [매일] 공지사항
-every :day, at: ['5:10 am'] do
+every :day, at: local('5:10 am') do
     rake "crawling_notice:crawling_notice"
 end
 
 # [1주일] 크롤링 기록 제거
-every 1.weeks, at: ['5:12 am'] do
+every 1.weeks, at: local('5:12 am') do
     rake "crawling_delete:crawling_delete"
 end
 
 # [매 한 달] 학사일정 가져오기 및 이전 달 크롤링 기록 제거
-every 1.month, at: ['3:00 am'] do
+every 1.month, at: local('3:00 am') do
     rake "crawling_univ_schedule:crawling_univ_schedule"
 end
 
