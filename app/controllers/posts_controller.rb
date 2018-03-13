8d4e8a6efb5aa01b28a502553b29bd765d4f3a58
ChulMin Kim <kbs4674@likelion.org>
2018-03-13 04:30:35 +0000

    추천시스템 오류 수정

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,11 +26,11 @@ class PostsController < ApplicationController
     #만약 '찬성' 투표를 이미 한 경우 : '찬성' 투표 취소
     if ((current_user.voted_up_on? @post) == true)
       @post.unliked_by current_user
+      redirect_to(request.referrer, :notice => '해당 글의 추천을 취소하셨습니다.')
     else
       @post.upvote_by current_user
+      redirect_to(request.referrer, :notice => '해당 글을 추천하셨습니다.')
     end
-    
-    redirect_back(fallback_location: root_path)
   end
  
   # 투표 관련 Controller 내용 : 반대
@@ -40,11 +40,11 @@ class PostsController < ApplicationController
     #만약 '반대' 투표를 이미 한 경우 : '반대' 투표 취소
     if ((current_user.voted_down_on? @post) == true)
       @post.undisliked_by current_user
+      redirect_to(request.referrer, :notice => '해당 글의 반대를 취소하셨습니다.')
     else
       @post.downvote_from current_user
+      redirect_to(request.referrer, :notice => '해당 글을 반대하셨습니다.')
     end
-   
-    redirect_back(fallback_location: root_path)
   end
 
 
--- app/controllers/qnas_controller.rb
+++ app/controllers/qnas_controller.rb
@@ -23,8 +23,10 @@ class QnasController < ApplicationController
     #만약 '찬성' 투표를 이미 한 경우 : '찬성' 투표 취소
     if ((current_user.voted_up_on? @qna) == true)
       @qna.unliked_by current_user
+      redirect_to(request.referrer, :notice => '해당 글의 추천을 취소하셨습니다.')
     else
       @qna.upvote_by current_user
+      redirect_to(request.referrer, :notice => '해당 글을 추천하셨습니다.')
     end
     redirect_back(fallback_location: root_path)
   end
@@ -36,8 +38,10 @@ class QnasController < ApplicationController
     #만약 '반대' 투표를 이미 한 경우 : '반대' 투표 취소
     if ((current_user.voted_down_on? @qna) == true)
       @qna.undisliked_by current_user
+      redirect_to(request.referrer, :notice => '해당 글의 반대를 취소하셨습니다.')
     else
       @qna.downvote_from current_user
+      redirect_to(request.referrer, :notice => '해당 글을 반대하셨습니다.')
     end
    
     redirect_back(fallback_location: root_path)
