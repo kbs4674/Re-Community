39ecd858beccefb40f6d84f41a743447b05b1682
ChulMin Kim <kbs4674@likelion.org>
2018-03-19 15:42:52 +0000

    production.rb 수정

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,8 +55,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://13.125.169.156/cable'
-  config.action_cable.allowed_request_origins = ['http://13.125.169.156']
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.allowed_request_origins = [ 'http://13.125.169.156' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
