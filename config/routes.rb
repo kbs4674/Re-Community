b66ebb0aeb81e64d79bf91432ec96ba79bb6ee17
ChulMin Kim <kbs4674@likelion.org>
2018-03-21 05:08:06 +0000

    메세지 뷰/메세지 알림 수정

--- config/routes.rb
+++ config/routes.rb
@@ -48,8 +48,6 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { omniauth_callbacks: 'users/omniauth_callbacks' }
   
   get '/users/message'
-  get 'search_user/:q' => 'users#search_user'
-  
   resources :conversations, only: [:create] do
     member do
       post :close
