0d6dd630b035b735e4e8b3093cf3081bf7df9ff2
ChulMin Kim <kbs4674@likelion.org>
2018-03-23 12:13:31 +0000

    식단 시간 수정

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,7 +7,7 @@ class MessagesController < ApplicationController
                 @new_notification = NewNotification.create! user: Conversation.find(params[:conversation_id]).sender,
                                                    content: "#{'<i class="far fa-envelope"></i>'.html_safe} #{current_user.nickname.truncate(15, omission: '...')} 님으로 부터 메세지가 왔습니다.",
                                                    link: request.referrer
-            
+                                                   
             elsif @message.user.nickname == @conversation.sender.nickname
                 @new_notification = NewNotification.create! user: Conversation.find(params[:conversation_id]).recipient,
                                                    content: "#{'<i class="far fa-envelope"></i>'.html_safe} #{current_user.nickname.truncate(15, omission: '...')} 님으로 부터 메세지가 왔습니다.",
