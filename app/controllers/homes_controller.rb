5a66b8ea51e275bad3c6d34508f99f3fd935ac62
ChulMin Kim <kbs4674@likelion.org>
2018-05-21 06:32:25 +0000

    ITX 시간표 기능 추가, 학교 버스시간표 제거

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,3 +1,7 @@
+require 'rubygems'
+require 'rest_client'
+require 'cgi'
+
 class HomesController < ApplicationController
     def index
         @bulletins = Bulletin.all
@@ -17,10 +21,12 @@ class HomesController < ApplicationController
 
         # *크롤링 목록들*
         #크롤링(nokogiri) : 열차 시간 조회(남춘천 출발)
+        
         #일반 경춘선
         doc5 = Nokogiri::HTML(open("http://m.seoul.go.kr/traffic/SubInfoNearDetail.do?subSearch=G&station=P139&upage=2&flag=3&sflag=1"), nil, 'euc-kr')
         subway = doc5.css('div#subArrInfo > ul.lst > li:nth-child(1) > a > div')
         @subway_time = subway.map { |cur| cur.text }
+        
         #급행 경춘선
         if (Time.zone.now.strftime("%H").to_i >= 0 && Time.zone.now.strftime("%H").to_i <= 9)
             doc6 = Nokogiri::HTML(open("http://k-subway.korail.com/station.do?op=timeTableDetail&stTrainNo=K8402&mainLine=G&inoutTag=1&weekTag=1&stationId=1328"), nil, 'utf-8')
@@ -30,5 +36,14 @@ class HomesController < ApplicationController
             subway_rapid = doc6.css('body > div > table > tr:nth-child(3) > td:nth-child(2)')
         end
         @subway_rapid_time = subway_rapid.map { |cur| cur.text }
+        
+        #ITX 기차
+        url = 'http://openapi.tago.go.kr/openapi/service/TrainInfoService/getStrtpntAlocFndTrainInfo?serviceKey=5gyn5FLcQFtbUEczDFKJjTIUWNw3%2FgNJRvCmb4uGDVT2kwrBO%2F7LSJoqM85znphfkenFzGf6e0WTU%2FeYsH%2FGKw%3D%3D'
+        headers = { :params => { CGI::escape('numOfRows') => '30', CGI::escape('depPlaceId') => 'NAT140840', CGI::escape('arrPlaceId') => 'NAT010032', CGI::escape('depPlandTime') => "#{Time.now.strftime('%Y%m%d')}", CGI::escape('trainGradeCode') => '09' } }
+        @itx_time_list = RestClient::Request.execute :method => 'GET', :url => url , :headers => headers
+        @itx_result = @itx_time_list.body
+        @doc = Nokogiri::XML(@itx_result)
+        @itx_dep_time = @doc.xpath("//depplandtime")
+        @itx_dep_time_result = @itx_dep_time.map { |cur| cur.text }
     end
 end
